832e6108348fe4a0815418eee43d923a1304325f
Malek <malek.a.husseini@gmail.com>
2022-03-29 12:45:42 +0200

    add heroku

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ import pandas as pd
 from starter.ml.model import load_model, inference
 from starter.ml.data import process_data
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull")!=0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 
 app = FastAPI()
 
